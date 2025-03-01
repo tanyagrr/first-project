3f8fdb896dfdca67c8293188dc197b9be5c540a8
root <root@DESKTOP-A0MIIF9.>
2025-03-01 17:27:36 +0200

    fixing illustration comp

--- src/components/Illustration.jsx
+++ src/components/Illustration.jsx
@@ -1,14 +1,13 @@
 function Illustration() {
     return (
-        <div className="illustration">
+        <><div className="illustration">
             <div className="cone light_blue" />
             <div className="cone yellow" />
             <div className="cone green" />
             <div className="cone dark_blue" />
             <div className="cone red" />
             <div className="cone purple" />
-            <div className="bottomline" />
-        </div>
+        </div><div className="bottomline" /></>
     )
 }
 
